90028b2a01e5e9df76a072e6e333c61039253e5f
Ben Monaghan <monoganog@gmail.com>
2023-02-22 02:01:10 +1100

    Making it work with Particles instead of GameObjects

--- Assets/ChaosManager.cs
+++ Assets/ChaosManager.cs
@@ -7,6 +7,8 @@ using DG.Tweening;
 
 public class ChaosManager : MonoBehaviour
 {
+    public ParticleSystem particles;
+
     public Transform currentLocation;
     [Header("Line Renderer")]
     public LineRenderer line;
@@ -36,11 +38,21 @@ public class ChaosManager : MonoBehaviour
     private bool paused = false;
     private Transform nextChosenTransform;
 
+    public bool canVisitPrevious = true;
+
+    private int lastVisittedIndex;
+
+    private int ammountOfParticles;
+
     // Start is called before the first frame update
     void Start()
     {
         //InvokeRepeating("FindNextPoint",0.5f,0.001f);
         InitialiseUI();
+
+
+
+
     }
 
     // Update is called once per frame
@@ -53,12 +65,19 @@ public class ChaosManager : MonoBehaviour
         if(currentTime>= iterationTime && !paused)
         {
             // Find the next point and reset the currentTime
-            FindNextPoint();
+
+               FindNextPoint();
+         
+            
             currentTime = 0;
         }
 
+
+        
+        
+
         // Ammount of itterations so far is equal to the amount of instantiated objects
-        itterationText.text = instantiatedObjects.Count.ToString();
+        itterationText.text = ammountOfParticles.ToString();
 
         // Calculation to display the frames per second without decimals
         fpsText.text = Mathf.RoundToInt(1.0f / Time.deltaTime).ToString();
@@ -70,6 +89,14 @@ public class ChaosManager : MonoBehaviour
         // Find a random number between 0 and the total amount of points
         int randomNum = Random.Range(0,points.Length);
 
+        if(!canVisitPrevious)
+        {
+            while(randomNum == lastVisittedIndex)
+            {
+                randomNum = Random.Range(0,points.Length);
+            }
+        }
+
         // Use the random number as the index of the points array that contains the corner points
         nextChosenTransform = points[randomNum];
 
@@ -78,6 +105,8 @@ public class ChaosManager : MonoBehaviour
 
         // Find the middle of the 2 chosen points
         FindMiddle();
+
+        lastVisittedIndex = randomNum;
     }
 
     private void FindMiddle()
@@ -85,8 +114,16 @@ public class ChaosManager : MonoBehaviour
         // Create and assign the value to the middle point to be the middle of the current position and the chosen corner point
         Vector3 middlePoint = (currentLocation.position + nextChosenTransform.position) / 2;
         // Instantiate an object at this middle point and add it to a list
-        GameObject instance = Instantiate(objectToInstantiate,middlePoint,Quaternion.identity);
-        instantiatedObjects.Add(instance);
+        //GameObject instance = Instantiate(objectToInstantiate,middlePoint,Quaternion.identity);
+        //instantiatedObjects.Add(instance);
+
+
+        var emitParams = new ParticleSystem.EmitParams();
+        emitParams.position = middlePoint;
+        
+        particles.Emit(emitParams, 1);
+
+        ammountOfParticles++;
 
         // Animate the current location to be at the last middle point
         currentLocation.DOMove(middlePoint,iterationTime);
@@ -133,6 +170,8 @@ public class ChaosManager : MonoBehaviour
 
     public void Stop()
     {
+        ammountOfParticles = 0;
+
         // Pause
         paused = true;
 
