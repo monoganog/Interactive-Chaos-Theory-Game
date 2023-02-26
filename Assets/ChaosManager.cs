6f2920a3547755b431e0e6e9dbf3a3628fccadc3
Ben Monaghan <monoganog@gmail.com>
2023-02-26 22:36:51 +1100

    New way to add sides to shapes

--- Assets/ChaosManager.cs
+++ Assets/ChaosManager.cs
@@ -22,6 +22,8 @@ public class ChaosManager : MonoBehaviour
 	public Slider itterationTimeSlider;
 	public TMPro.TextMeshProUGUI itterationText;
 	public TMPro.TextMeshProUGUI fpsText;
+	
+	public TMPro.TMP_InputField sidesInput;
 
 	private Button removeButton;
 	// Private Variables
@@ -34,6 +36,8 @@ public class ChaosManager : MonoBehaviour
 	public bool canVisitPreviousVertex = true;
 
 	public bool canBeOnePlaceAwayClockwise = true;
+	
+	public bool canBeTwoPlacesAwayFromPrevious = true;
 
 
 	private int lastVisittedIndex;
@@ -47,9 +51,11 @@ public class ChaosManager : MonoBehaviour
 	void Start()
 	{
 		//InitialiseUI();
-		removeButton = GameObject.Find("Remove").GetComponent<Button>();
+		//removeButton = GameObject.Find("Remove").GetComponent<Button>();
 		
-		LoadSerpinskyTrianglePreset();
+		//LoadSerpinskyTrianglePreset();
+		
+		//CreateShape(9);
 	}
 
 	// Update is called once per frame
@@ -76,7 +82,7 @@ public class ChaosManager : MonoBehaviour
 	// Method to find the next corner vertex to be selected
 	public void FindNextVertex(){
 
-		// The incex of the next vetex is a random number between 0 and the total amount of vertices
+		// The index of the next vetex is a random number between 0 and the total amount of vertices
 		int indexOfNextVertex = Random.Range(0,vertices.Count);
 
 		// If not allowed to visit the previous vertex
@@ -97,6 +103,26 @@ public class ChaosManager : MonoBehaviour
 			// While the next vertex to be chosen is == next cloxkwise element, reroll
 			while(indexOfNextVertex == nextClockwiseElementIndex)
 			{
+				indexOfNextVertex = Random.Range(0,vertices.Count);
+			}
+		}
+		
+		// Next Vertex cannot be two places away from the previous, meaning the next vertex selected must be either the same, or one either side of the last chosen vertex
+		if(!canBeTwoPlacesAwayFromPrevious)
+		{
+			
+			int nextClockwiseElementIndex = (lastVisittedIndex + 1) % vertices.Count;
+			int nextAntiClockwiseElementIndex = (lastVisittedIndex - 1 + vertices.Count) % vertices.Count;
+			
+			Debug.Log("last visited: " + lastVisittedIndex + "  Next Clockwise: " + nextClockwiseElementIndex + "  Next anti clockwise: "+ nextAntiClockwiseElementIndex);
+			
+			//int twoElementsAway = (lastVisittedIndex + 2) % vertices.Count;
+			
+			// Basically we want the next index to either be one away from the last index or the last index. If its not then reroll
+			while(indexOfNextVertex != nextClockwiseElementIndex && indexOfNextVertex != nextAntiClockwiseElementIndex && indexOfNextVertex != lastVisittedIndex)
+			//while(indexOfNextVertex == twoElementsAway)
+			{
+				// Reroll
 				indexOfNextVertex = Random.Range(0,vertices.Count);
 			}
 		}
@@ -173,6 +199,60 @@ public class ChaosManager : MonoBehaviour
 		particles.Clear();
 		HideLineRenderer();
 	}
+	
+	public void CreateShape()
+	{
+		int sides = int.Parse(sidesInput.text.ToString());
+		RemoveAllVertices();
+		
+		// Add vertices
+		for(int i = 0; i < sides; i++)
+		{
+			AddVertex();
+		}
+		// Calculate angle between each vertex
+		float angle = 2 * Mathf.PI / sides; // 72°
+		
+		// Itterate over each vertex
+		for (int i = 0; i < sides; i++)
+		{	
+			// Assign position to each vertex
+			vertices[i].position  = new Vector3
+			(
+				Mathf.Cos(-angle * i + Mathf.PI/2),
+				Mathf.Sin(-angle * i + Mathf.PI/2),
+				0
+			) 	* scaleFactor;
+		}
+	}
+	// Method overloading for calling the CreateShape method internally with arguments rather than via UI
+	public void CreateShape(int sides)
+	{
+		
+		RemoveAllVertices();
+		
+		// Add vertices
+		for(int i = 0; i < sides; i++)
+		{
+			AddVertex();
+		}
+		// Calculate angle between each vertex
+		float angle = 2 * Mathf.PI / sides; // 72°
+		
+		// Itterate over each vertex
+		for (int i = 0; i < sides; i++)
+		{	
+			// Assign position to each vertex
+			vertices[i].position  = new Vector3
+			(
+				Mathf.Cos(-angle * i + Mathf.PI/2),
+				Mathf.Sin(-angle * i + Mathf.PI/2),
+				0
+			) 	* scaleFactor;
+		}
+	}
+	
+	
 
 	public void HideLineRenderer()
 	{
@@ -197,7 +277,7 @@ public class ChaosManager : MonoBehaviour
 		// Spawn new vertex
 		GameObject instance = Instantiate(vertexPrefab);
 		// Apply a random Offset
-		Vector3 randomOffset = new Vector3(Random.Range(-5,6),Random.Range(-5,6),0);
+		Vector3 randomOffset = new Vector3(0,Random.Range(-5,6),0);
 		instance.transform.position += randomOffset;
 		
 		// Add new vertex to the vertices list
@@ -206,14 +286,14 @@ public class ChaosManager : MonoBehaviour
 		// Update the UI on the vertex to be representitive of the count
 		instance.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "V"+ vertices.Count.ToString();
 
-		// If there is 3 vertices make the remove button work
+		// If there is 3 vertices make the remove button interactable
 		if(vertices.Count==3)
 		{
-			removeButton.interactable = true;
+			//removeButton.interactable = true;
 		}
 	}
 
-	// Adds a new vertex, called Via UI Button
+	// Removes a vertex, called Via UI Button
 	public void RemoveVertex()
 	{
 		// Check to see if theres atleast 2 Vertices
@@ -224,10 +304,10 @@ public class ChaosManager : MonoBehaviour
 			// Remove it from the list
 			vertices.RemoveAt(vertices.Count-1);
 
-			// If theres only two vertices then make the remove button not work
+			// If theres only two vertices then make the remove button not interactable
 			if(vertices.Count==2)
 			{
-				removeButton.interactable = false;
+				//removeButton.interactable = false;
 			}
 		}
 	}
@@ -243,21 +323,7 @@ public class ChaosManager : MonoBehaviour
 	
 	public void LoadSerpinskyTrianglePreset()
 	{
-		RemoveAllVertices();
-		
-		// Add three vertices
-		for(int i = 0; i < 3; i++)
-		{
-			AddVertex();
-		}
-		
-		// This for loop iterates 3 times and sets the position of each vertex in a regular triangle pattern by calculating
-		// the angle of each vertex relative to the center using the scaleFactor as its radius.
-		for (int i = 0; i < 3; i++)
-		{
-			float angle = i * Mathf.PI * 2.0f / 3.0f + (Mathf.PI / 2f);
-			vertices[i].position = new Vector3(Mathf.Cos(angle) * scaleFactor, Mathf.Sin(angle) * scaleFactor,0);
-		}
+		CreateShape(3);
 		
 		// Set other params
 		canBeOnePlaceAwayClockwise = true;
@@ -266,19 +332,7 @@ public class ChaosManager : MonoBehaviour
 	
 	public void LoadRestrictedSquarePreset()
 	{
-		RemoveAllVertices();
-		
-		// Add four vertices
-		for(int i = 0; i < 4; i++)
-		{
-			AddVertex();
-		}
-		
-		// Position them in a square
-		vertices[0].position = new Vector3(-scaleFactor, scaleFactor	,0);
-		vertices[1].position = new Vector3(	scaleFactor, scaleFactor	,0);
-		vertices[2].position = new Vector3(	scaleFactor, -scaleFactor	,0);
-		vertices[3].position = new Vector3(-scaleFactor, -scaleFactor	,0);
+		CreateShape(4);
 		
 		// Set other params
 		canBeOnePlaceAwayClockwise = true;
@@ -287,8 +341,8 @@ public class ChaosManager : MonoBehaviour
 	
 	public void LoadRestrictedClockwiseSquarePreset()
 	{
-		// First start with a square
-		LoadRestrictedSquarePreset();
+		
+		CreateShape(4);
 		
 		// Then alter these params
 		canBeOnePlaceAwayClockwise = false;
@@ -297,31 +351,6 @@ public class ChaosManager : MonoBehaviour
 	
 	public void LoadPentagonPreset()
 	{
-		RemoveAllVertices();	
-		// Add five vertices
-		for(int i = 0; i < 5; i++)
-		{
-			AddVertex();
-		}		
-		
-		// First calculate the angle of 72°
-		float angle = 2 * Mathf.PI / 5; // 72°
-		
-		// Itterate over each vertex
-		for (int i = 0; i < 5; i++)
-		{
-			// The formular for the position of the vertices in a regular pentigon is as follows:
-			// for the X part of the vector Cos of the angle multiplied by the index i 
-			// with an offset of pi/2 to ensure the base is perpendicular the floor. For the Y part of the Vector it is the same except using Sin.
-			// The angle is also negative here so that the points are placed in a clockwise order. Finally the 
-			// whole vector is multiplied by the scaling factor to make it larger
-			
-			vertices[i].position  = new Vector3
-			(
-				Mathf.Cos(-angle * i + Mathf.PI/2),
-				Mathf.Sin(-angle * i + Mathf.PI/2),
-				0
-			) 	* scaleFactor;
-		}
+		CreateShape(5);
 	}
 }
